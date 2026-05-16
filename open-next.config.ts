043b2dea50e43c19dc1b87954c6bb650a082dc5f
armazempreta13 <armazempreta13@users.noreply.github.com>
2026-05-16 03:26:19 -0300

    Fix open-next config type error

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,6 +5,5 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 export default defineCloudflareConfig({
 	// For best results consider enabling R2 caching
 	// See https://opennext.js.org/cloudflare/caching for more details
-	// incrementalCache: r2IncrementalCache,
-	buildCommand: "npx next build",
+	// incrementalCache: r2IncrementalCache
 });
